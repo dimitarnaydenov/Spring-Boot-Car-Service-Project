46a085df45c6642a7bc2d8a39b36798d6aa8b4a4
Dimitar Naydenov <mitkoik@gmail.com>
2023-02-02 17:14:00 +0200

    editVehicle implementation

--- src/main/java/com/carService/service/VehicleService.java
+++ src/main/java/com/carService/service/VehicleService.java
@@ -4,6 +4,7 @@ import com.carService.model.Vehicle;
 import com.carService.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -24,5 +25,33 @@ public class VehicleService {
 
     public Vehicle addVehicle(Vehicle vehicle){
         return vehicleRepository.save(vehicle);
+    }
+
+    public Vehicle editVehicle(String brand, String model, String registrationNumber, int productionYear, int id) {
+
+        Vehicle vehicle = vehicleRepository.findById(id).orElse(null);
+
+        if(vehicle != null){
+            if (!brand.equals("") && !vehicle.getBrand().equals(brand)) {
+                vehicle.setBrand(brand);
+            }
+
+            if (!model.equals("") && !vehicle.getModel().equals(model)) {
+                vehicle.setModel(model);
+            }
+
+            if (!registrationNumber.equals("") && !vehicle.getRegistrationNumber().equals(registrationNumber)) {
+                vehicle.setRegistrationNumber(registrationNumber);
+            }
+
+            if (productionYear != 0 && !vehicle.getProductionYear().equals(productionYear)) {
+                vehicle.setProductionYear(productionYear);
+            }
+
+            return vehicleRepository.save(vehicle);
+        }
+
+        return null;
+
     }
 }
